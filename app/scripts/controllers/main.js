cc40f740b4e57e02414cdd82a52dd6a92d6cf923
Greg Weisman <grwgreg@gmail.com>
2014-02-01 14:11:45 -0500

    card matrix offsuit bindings and styled

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -75,7 +75,7 @@ angular.module('flopzillaApp')
               ng-class="{pressed : cards.on}" ng-click="toggleOn()"> \
               {{tag}}</button>',
     controller: function($scope) { //link works here too
-      $scope.toggleOn = function() {
+      $scope.toggleOn = function() {                                                   //greg add toggleall for suited!
         $scope.cards.on = !$scope.cards.on;
         console.log($scope.cards);
       }
@@ -88,12 +88,64 @@ angular.module('flopzillaApp')
     restrict: 'E',
     replace: true,
     //template: '<span ng-click="add()">{{it }}</span>',
-    template: ['<button type="button" class="btn btn-xs"',
-              'ng-click="deb()">DEBUG</button>'].join(''),
+    template: function() { return['<button type="button" class="btn btn-xs"',
+              'ng-click="deb()">DEBUG</button>'].join('')},
     controller: function($scope) { //link works here too
       $scope.deb = function() {
         console.log($scope.ranges);
       }
+    }
+  };
+})
+
+//put card matrix logic in a factory too
+.directive('cardMatrix', function() {
+  var cardCols = '23456789TJQKA'.split(''),
+    cardRows = cardCols.slice().reverse(),
+    matrix = [];
+
+  _.each(cardCols, function(colCard, colIndex) {
+    var row = [];
+    _.each(cardRows, function(rowCard, rowIndex) {
+      var type = '',
+      printType = '';
+      if (colIndex + rowIndex > 12) type = 's';
+      if (colIndex + rowIndex === 12) type = 'p';
+      if (colIndex + rowIndex < 12) type = 'o';
+      var tag = (type == 's') ? colCard + rowCard : rowCard + colCard;
+      row.push({cards: tag, type: type});
+    });
+    matrix.push(row);
+  });
+  matrix = matrix.reverse();
+  console.log(matrix);
+
+  return {
+    restrict: 'E',
+    //replace: true,
+    compile: function(el, attr) {
+      var matrixString = '<ul class="card-matrix">'; 
+      _.each(matrix, function(row) {
+        matrixString += '<li>';
+        _.each(row, function(cardData) {
+          var hand = 'ranges.' + cardData.cards,
+            printType = (cardData.type === 'p') ? '' : cardData.type,
+            tag = cardData.cards + printType,
+            color = '';
+          if (cardData.type == 's') color = 'btn-info'; 
+          if (cardData.type == 'p') color = 'btn-success'; 
+          if (cardData.type == 'o') color = 'btn-primary'; 
+          matrixString += '<preflop-hand '; 
+          matrixString += "cards='" + hand + "' ";
+          matrixString += "tag='" + tag + "' ";
+          matrixString += "color='" + color + "'>"; 
+          matrixString += "</preflop-hand>";
+        }); 
+        matrixString += '</li>';
+      });
+      matrixString += '</ul>';
+      //el.html(matrixString);
+      el.replaceWith(matrixString);
     }
   };
 })
